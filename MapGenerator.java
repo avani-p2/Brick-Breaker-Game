3ed5a81f95dcfa5062ff6994f737804961f2fc5a
avani-p2 <avanipayasi7@gmail.com>
2024-09-16 19:16:36 +0530

    Create MapGenerator.java
    Adding MapGenerator.java

---
+++ MapGenerator.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package brick.breaker.game;
+
+/**
+ *
+ * @author AVANI PAYASI
+ */
+import java.awt.*;
+public class MapGenerator 
+{
+    public int map[][];
+    public int brickWidth;
+    public int brickHeight;
+    public MapGenerator(int row, int col)//constructor
+    {
+        map=new int[row][col];
+        for(int map1[]:map)
+        {
+            for(int j=0; j<map[0].length ;j++)
+            {
+                map1[j]=1;
+            }
+        }
+        brickWidth=540/col;
+        brickHeight=150/row;
+    }
+    
+    public void draw(Graphics2D g)
+    {
+        for(int i=0;i<map.length;i++)
+        {
+            for(int j=0;j<map[0].length;j++)
+            {
+                if(map[i][j]>0)
+                {
+                    g.setColor(Color.magenta);
+                    g.fillRect(j*brickWidth+80,i*brickHeight+50,brickWidth,brickHeight);
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.cyan); 
+                    g.drawRect(j*brickWidth+80,i*brickHeight+50,brickWidth,brickHeight);
+                    
+                }
+            }
+        }
+    }
+    
+    public void setBrickValue(int value,int row,int col)
+    {
+        map[row][col]=value;
+    }
+}
